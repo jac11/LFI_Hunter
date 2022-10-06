9ae4e78580d7d5c457ebe59159fd1a2d3000d655
jac11 <jac11devel@gmail.com>
2022-10-06 09:54:21 -0400

    Add files via upload

--- LFI_Hunter.py
+++ LFI_Hunter.py
@@ -6,7 +6,7 @@ import sys
 from Package.main_lfi import Local_File_In
 from Package.aggressiv import Aggressiv
 class Hannter_LFI:
-           
+      
        def __init__(self):
          self.control()
          if self.args.aggress:
@@ -14,25 +14,35 @@ class Hannter_LFI:
          else:    
              run = Local_File_In()
        def control(self): 
+         try: 
            parser = argparse.ArgumentParser(description="Usage: [OPtion] [arguments] [ -w ] [arguments]") 
            parser.add_argument("-UV ","--Vulnurl"     , action=None         ,required=True     ,help ="url Targst web") 
-           parser.add_argument("--auth"               , action='store_true'                    ,help ="url Targst web") 
+           parser.add_argument("--auth"               , action='store_true'                    ,help ="auth mautrd web") 
            parser.add_argument("-F","--filelist"      , action=None                            ,help ="read fron lfi wordlsit ")
-           parser.add_argument("-C","--Cookie"        , action=None                            ,help ="Login sesion Cookie")  
-           parser.add_argument("-B64","--base64"      , action='store_true'                    ,help ="Login sesion base64")  
-           parser.add_argument("-R","--read"          , action=None                            ,help ="Login sesion base64")  
+           parser.add_argument("-C","--Cookie"        , action=None        ,required=True      ,help ="Login sesion Cookie")  
+           parser.add_argument("-B64","--base64"      , action='store_true'                    ,help ="decode filter php  base64")  
+           parser.add_argument("-R","--read"          , action=None                            ,help ="use to read file on the traget machine")  
            parser.add_argument("-UF ","--UserForm"    , action=None                            ,help =" add name of the HTML Form Login User")
            parser.add_argument("-PF ","--PassForm"    , action=None                            ,help ="add name of the HTML Form Login Passord")
            parser.add_argument("-P  ","--password"    , action=None                            ,help ="use specific Passowrd")   
-           parser.add_argument("-LU  ","--loginurl"   , action=None                            ,help ="use specific Passowrd") 
+           parser.add_argument("-LU  ","--loginurl"   , action=None                            ,help =" add login url for auth motted") 
            parser.add_argument("-U  ","--user"        , action=None                            ,help ="use specific username ")
-           parser.add_argument("-A","--aggress"     ,action='store_true'                    ,help ="use specific username ")
+           parser.add_argument("-A","--aggress"     ,action='store_true'                        ,help ="  use aggressiv mode  ")
            self.args = parser.parse_args()     
            if len(sys.argv)!=1 :
               pass
            else:
               parser.print_help()         
-              exit()                   
+              exit()
+         except AssertionError as a :
+            print('\n'+'='*20+"\n[*] ERROR-INFO "+'\n'+'='*30+'\n')
+            print("[*] Error :  Bad argument" )
+            print('\n'+'='*10+"\n[*] Solution "+'\n'+'='*14+'\n')
+            print("[*] try to use --help")
+            print("[*] ckeck Readme file at : https://www.github/jac11/LFI_Hunter.git")
+            exit()
+            
+            exit()                
 if __name__=='__main__':
     Hannter_LFI()
 
