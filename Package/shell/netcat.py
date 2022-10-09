e5711d22edf660d34cfb418c734b93d46398d6f4
jac11 <jac11devel@gmail.com>
2022-10-09 15:25:42 -0400

    Create netcat.py

---
+++ Package/shell/netcat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os
+with open (str(os.getcwd())+'/Package/Banner','r') as read:
+     print(read.read())
+info = ''
+info +='='*20+'\n'
+info +='[+] netcat lislner \n'
+info +='='*30+'\n'
+info +='[!] IP   : .............| : 0.0.0.0\n'
+info +='[+] Port : .............| : 7777\n'
+info +='='*40+'\n'
+print(info)
+line = '┌──(@jacstory)-[~/LFT_Hunter]\n'
+line +='└─$ \n'
+print(line)
+line1 ='nc -nvlp  7777'
+command = line1
+print(command)
+command = os.system(command)
+
+   
+   
