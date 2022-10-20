aeca9f4d14b1fe4bd0d68a0e1a7e079f5e063c33
jac11 <jac11devel@gmail.com>
2022-10-19 20:53:15 -0400

    Add files via upload

--- Package/LFT_Shell_connect.py
+++ Package/LFT_Shell_connect.py
@@ -95,11 +95,11 @@ class Shell_conncet:
                          path   =  "python " +str(os.getcwd())+'/Package/shell/ssh.py'
                          run    =  'gnome-terminal  -e '+'" '+path+' "' 
                          xterm  = subprocess.call( run ,shell=True,stderr=subprocess.PIPE)
-                    for T in range(50):
+                    for T in range(30):
                         for C in  os.popen(" ps ax | grep ssh.py | grep -v grep") :
-                            if "ssh.py" in C  and T != 6 :
+                            if "ssh.py" in C  and T != 29 :
                                time.sleep(4)  
-                            elif  T == 6 :
+                            elif  T == 29 :
                                 for line in os.popen("ps ax | grep ssh.py  | grep -v grep"):
                                     fields = line.split()
                                     pid = fields[0]
@@ -149,20 +149,21 @@ class Shell_conncet:
                 except KeyboardInterrupt:
                      exit()      
      def control(self): 
-           parser = argparse.ArgumentParser(description="Usage: [OPtion] [arguments] [ -w ] [arguments]") 
-           parser.add_argument("-UV ","--Vulnurl"     , action=None         ,required=True     ) 
-           parser.add_argument("--auth"               , action='store_true'                    ) 
-           parser.add_argument("-F","--filelist"      , action=None                            )
-           parser.add_argument("-C","--Cookie"        , action=None          ,required=True    )  
-           parser.add_argument("-B64","--base64"      , action='store_true'                    )  
-           parser.add_argument("-R","--read"          , action=None                            )  
-           parser.add_argument("-UF ","--UserForm"    , action=None                            )
-           parser.add_argument("-PF ","--PassForm"    , action=None                            )
-           parser.add_argument("-P  ","--password"    , action=None                            )   
-           parser.add_argument("-LU  ","--loginurl"   , action=None                            ) 
-           parser.add_argument("-U  ","--user"        , action=None                            )
-           parser.add_argument( "-S", "--shell"       , action=None                            )
-           self.args = parser.parse_args()
+           parser = argparse.ArgumentParser(description="Usage: [OPtion] [arguments] [ -w ] [arguments]")             
+           parser.add_argument("-UV","--Vulnurl"     , action=None         ,required=True     ,help ="url Targst web") 
+           parser.add_argument("--auth"               , action='store_true'                    ,help ="auth mautrd web") 
+           parser.add_argument("-F","--filelist"      , action=None                            ,help ="read fron lfi wordlsit ")
+           parser.add_argument("-C","--Cookie"        , action=None        ,required=True      ,help ="Login sesion Cookie")  
+           parser.add_argument("-B","--base64"      , action='store_true'                    ,help ="decode filter php  base64")  
+           parser.add_argument("-R","--read"          , action=None                            ,help ="use to read file on the traget machine")  
+           parser.add_argument("-UF","--UserForm"    , action=None                            ,help =" add name of the HTML Form Login User")
+           parser.add_argument("-PF","--PassForm"    , action=None                            ,help ="add name of the HTML Form Login Passord")
+           parser.add_argument("-P","--password"    , action=None                            ,help ="use specific Passowrd")   
+           parser.add_argument("-LU","--loginurl"   , action=None                            ,help =" add login url for auth motted") 
+           parser.add_argument("-U","--user"        , action=None                            ,help ="use specific username ")
+           parser.add_argument("-A" ,"--aggress"       ,action='store_true'                     ,help ="  use aggressiv mode  ")
+           parser.add_argument( "-S", "--shell"       , action=None                            ,help ="  to connent reverseshell   ")
+           self.args = parser.parse_args()    
            if len(sys.argv)!=1 :
               pass
            else:
