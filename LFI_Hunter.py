f7db98ee44729a33a43c1f2aa59ddb055a59b0f3
jac11 <jac11devel@gmail.com>
2024-10-15 02:05:14 +0300

    Add files via upload

--- LFI_Hunter.py
+++ LFI_Hunter.py
@@ -2,26 +2,63 @@
 
 import argparse
 import sys
-
-from Package.main_lfi import Local_File_In
-from Package.aggressiv import Aggressiv
 with open('./Package/Banner','r') as banner:
-     print(banner.read())
+   print(banner.read())
+
 class Hannter_LFI:
       
-       def __init__(self):
+      def __init__(self):
          self.control()
+         
          if self.args.readuser:
-            with open(self.args.readuser,'r') as username:
-                 self.args.user = username.read().replace('/n','')
+             with open(self.args.readuser,'r') as username:
+                  self.args.user = username.read().replace('/n','')
          if self.args.readpass:
+
             with open(self.args.readpass,'r') as password:
-                 self.args.password = password.read().replace('/n','')              
+               self.args.password = password.read().replace('/n','')  
+
          if self.args.aggress:
-             run =Aggressiv()
+               from Package.aggressiv import Aggressiv  
+
+               if self.args.auth and self.args.Vulnurl\
+                  and self.args.password and self.args.user\
+                  and self.args.Cookie and self.args.loginurl\
+                  and self.args.aggress :  
+
+                     Aggressiv.__init__(self,args = self.control)          
+                     Aggressiv.Login_auth(self,args = self.control)
+                     Aggressiv.url_request(self,args = self.control)
+                     Aggressiv.Scan_result(self,aegs = self.control) 
+
+               elif not self.args.auth and self.args.Vulnurl\
+               and not self.args.password and not self.args.user and self.args.Cookie\
+               and self.args.aggress :  
+
+                  Aggressiv.__init__(self,args = self.control)
+                  Aggressiv.url_request(self,args = self.control)
+                  Aggressiv.Scan_result(self,aegs = self.control)
+
+               else:
+                  print('\n'+'='*20+"\n[*] ERROR-INFO "+'\n'+'='*30+'\n')
+                  print("[*] Error :  Bad argument Logic command  Error" )
+                  print('\n'+'='*10+"\n[*] Solution "+'\n'+'='*14+'\n')
+                  print('[+] To use LFI with login     : --auth --loginurl --Vulnurl --user --password --filelist --Cookie ') 
+                  print('[+] To use LFI without  login : --Vulnurl --filelist --Cookie') 
+                  print("[*] ckeck Readme file at      : https://www.github/jac11/LFI_Hunter.git")
+                  exit()  
+         elif self.args.fizzing:
+              from Package.main_lfi import Local_File_In
+              Local_File_In.__init__(self,args = self.control)
+              Local_File_In.url_request(self,args = self.control)
+              Local_File_In.Scan_result(self,aegs = self.control)
+              if self.args.shell and self.args.port:
+                 Local_File_In.Reverse_shell(self,args = self.control)
+                                         
          else:    
-             run = Local_File_In()       
-       def control(self): 
+             print("else")
+             exit()    
+      def control(self): 
          try: 
            parser = argparse.ArgumentParser(description="Usage: [OPtion] [arguments] [ -w ] [arguments]")             
            parser.add_argument("-UV","--Vulnurl"    , action=None         ,required=False      ,help ="url Targst web") 
@@ -41,6 +78,7 @@ class Hannter_LFI:
            parser.add_argument("--port"             ,action=None                             ,help ="  set port for netcat ")
            parser.add_argument("-D","--Domain"      ,action=None                             ,help ="  use target url domain not as ip 'http://www.anyDomain.com'")
            parser.add_argument("-S","--shell"       , action=None                            ,help ="  to connent reverseshell   ")
+           parser.add_argument("-Z","--fizzing"    , action='store_true'                      ,help ="  to connent reverseshell   ")
            self.args = parser.parse_args()     
            if len(sys.argv)!=1 :
               pass
@@ -52,10 +90,11 @@ class Hannter_LFI:
             print("[*] Error :  Bad argument" )
             print('\n'+'='*10+"\n[*] Solution "+'\n'+'='*14+'\n')
             print("[*] try to use --help")
-            print("[*] ckeck Readme file at : https://www.github/jac11/LFI_Hunter.git")
+            print("[*] Check Readme file at : https://www.github/jac11/LFI_Hunter.git")
             exit()
             
             exit()                
 if __name__=='__main__':
     Hannter_LFI()
 
+9
